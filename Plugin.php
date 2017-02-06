7b2f301c9fae9d27c96789a68c3387d6bfd1b39a
Patrick95 <lawcy@qq.com>
2017-02-06 15:37:11 +0800

    update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -1,16 +1,56 @@
-<?php 
-
-/* 激活插件方法 */
-public static function activate(){}
+<?php
+/**
+ * 多说评论导入typecho插件
+ * 
+ * @package duoshuo2typecho
+ * @author Patrick95
+ * @version 1.0.0
+ * @link https://muguang.me/
+ */
+class duoshuo2typecho_Plugin implements Typecho_Plugin_Interface
+{
+    /**
+     * 激活插件方法,如果激活失败,直接抛出异常
+     * 
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function activate()
+    {
+    
  
-/* 禁用插件方法 */
-public static function deactivate(){}
- 
-/* 插件配置方法 */
-public static function config(Typecho_Widget_Helper_Form $form){}
- 
-/* 个人用户的配置方法 */
-public static function personalConfig(Typecho_Widget_Helper_Form $form){}
- 
-/* 插件实现方法 */
-public static function render(){}
+    }
+    
+    /**
+     * 禁用插件方法,如果禁用失败,直接抛出异常
+     * 
+     * @static
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function deactivate(){}
+    
+    /**
+     * 获取插件配置面板
+     * 
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form 配置面板
+     * @return void
+     */
+    public static function config(Typecho_Widget_Helper_Form $form)
+    {
+       
+    }
+    
+    /**
+     * 个人用户的配置面板
+     * 
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form
+     * @return void
+     */
+    public static function personalConfig(Typecho_Widget_Helper_Form $form){}
+  
+}
