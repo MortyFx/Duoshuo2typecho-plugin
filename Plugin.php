f8d4d247ef3522f327a8c59b4d0e6edd0003d883
Patrick95 <lawcy@qq.com>
2017-02-06 14:37:05 +0800

    update Plugin.php

---
+++ Plugin.php
@@ -0,0 +1,16 @@
+<?php 
+
+/* 激活插件方法 */
+public static function activate(){}
+ 
+/* 禁用插件方法 */
+public static function deactivate(){}
+ 
+/* 插件配置方法 */
+public static function config(Typecho_Widget_Helper_Form $form){}
+ 
+/* 个人用户的配置方法 */
+public static function personalConfig(Typecho_Widget_Helper_Form $form){}
+ 
+/* 插件实现方法 */
+public static function render(){}
